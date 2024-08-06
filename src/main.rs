e73ae9eb1256feabc7e96219e3aa2dd5fb291f9c
nimaaskarian <nimaaskarian.work@outlook.com>
2024-08-06 07:54:20 +0330

    Forge cd into the code Now ye can dig through directories as easy as hammering a nail!

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::{io::{self, Write}, process::Command};
+use std::{env::{self},io::{self, Write}, path::{self, Path, PathBuf}, process::Command};
+
+use home::{home_dir};
 
 fn main() {
     loop {
@@ -6,14 +8,38 @@ fn main() {
         io::stdout().flush();
         let mut input = String::new();
         io::stdin().read_line(&mut input);
-        let mut parts = input.split_whitespace();
-        let command = parts.next();
-        if let Some(command) = command {
-            let mut child = Command::new(command)
-                .args(parts)
-                .spawn()
-                .unwrap();
-            child.wait();
+        command_handler(input);
+    }
+}
+
+fn command_handler(input: String) -> io::Result<()>{
+    let mut parts = input.split_whitespace().filter(|s| !s.is_empty());
+    if let Some(command) = parts.next() {
+        match command {
+            "cd" => {
+                if let Some(dir) = parts.next() {
+                    let dir = path::absolute(dir)?;
+                    if PathBuf::from(&dir).is_dir() {
+                        env::set_current_dir(&dir)?;
+                    } else {
+                        eprintln!("Blast it! The directory '{}' be missing!", dir.to_str().unwrap());
+                    }
+                } else {
+                    env::set_current_dir(home_dir().unwrap());
+                }
+            }
+            command => {
+                run_command(command, parts)
+            }
         }
     }
+    Ok(())
+}
+
+fn run_command<'a>(command: &'a str, parts: impl Iterator<Item=&'a str>) {
+    let mut child = Command::new(command)
+        .args(parts)
+        .spawn()
+        .unwrap();
+    child.wait();
 }
